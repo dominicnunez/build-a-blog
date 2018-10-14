9e536f18b27119a90665b5f6c6f177753d010f7b
Dominic <dominic.nunez@gmail.com>
2018-10-14 16:35:17 -0500

    added templates, main.py and readme

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    body = db.Column(db.String(2100))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+# The /blog route displays all the blog posts.
+
+# You're able to submit a new post at the /newpost route. 
+# After submitting a new post, your app displays the main blog page.
+
+# You have two templates, one each for the /blog (main blog listings) and /newpost (post new blog entry) views. 
+# Your templates should extend a base.html template which includes some boilerplate HTML that will be used on each page.
+
+# In your base.html template, you have some navigation links that link to the main blog page and to the add new blog page.
+
+# If either the blog title or blog body is left empty in the new post form, the form is rendered again, with a helpful error message and any previously-entered content in the same form inputs.
+@app.route('/blog', methods=['POST', 'GET'])
+def index():
+
+    if request.method == 'POST':
+
+
+
+@app.route('/newpost', methods=['POST'])
+
+
+
+if __name__ == '__main__':
+    app.run()
